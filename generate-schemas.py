1043732c84ebfd0f096b28d6ca2db3ffd4e4610f
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2018-08-13 17:11:32 +0100

    feat(initial): Initial commit
    * Start of bid-request.json
    * Spike of generate-schemas.py using a visitor-ish pattern

---
+++ generate-schemas.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import json
+
+
+class AthenaSchemaFromJson(object):
+
+    def __init__(self, file_name):
+        with open(file_name) as schema:
+            self.json = json.load(schema)
+
+    @staticmethod
+    def write(msg):
+        print(msg, end='')
+
+    def process_list(self, spec):
+        return ",".join(["%s" % (self.get_type(key)) for key in spec])
+
+    def process_dict(self, spec):
+        return ",".join(["%s:%s" % (key, self.get_type(spec[key])) for key in spec])
+
+    def get_type(self, e):
+        if isinstance(e, dict):
+            return "struct<%s>" % self.process_dict(e)
+        elif isinstance(e, list):
+            return "array<%s>" % self.process_list(e)
+        else:
+            return e
+
+    def generate(self):
+        print(self.get_type(self.json))
+
+
+if __name__ == '__main__':
+    AthenaSchemaFromJson('bid-request.json').generate()
